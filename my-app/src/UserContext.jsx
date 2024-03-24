82cca7537d42bfdbd9ca8135b75e4bc9c0b51352
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-03-24 03:46:14 -0400

    update

---
+++ my-app/src/UserContext.jsx
@@ -0,0 +1,30 @@
+import React, { useState, createContext, useContext } from 'react';
+
+// Create Context
+const UserContext = createContext();
+
+export const useUser = () => {
+    return useContext(UserContext)
+};
+
+// Provider Component
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        nationality: '',
+        language: '',
+        bio: '',
+    });
+
+    // Function to update user data
+    const updateUser = (userData) => {
+        setUser(userData);
+    };
+
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    );
+};
