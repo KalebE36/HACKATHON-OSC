c11faf269a8172706e7e04bd39af38b35986eab3
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-03-24 03:12:34 -0400

    signup now works

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -2,41 +2,6 @@ import React, { useState } from 'react';
 import './Login.css';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Example: Adjust the URL to where your Flask backend is hosted
-    const url = 'http://localhost:5000/signup';
-    const userData = {
-      username,
-      password,
-      email, // Add form fields to capture these as needed
-      nationality: '',
-      language: '',
-      bio: ''
-    };
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-      });
-
-      if (!response.ok) throw new Error('Signup failed');
-
-      // Handle success, perhaps redirect to login page or display a success message
-      console.log('Signup successful');
-    } catch (error) {
-      console.error('Error during signup:', error);
-    }
-  };
 
   return (
     <div className="login-page">
@@ -45,15 +10,15 @@ const LoginPage = () => {
       </div>
       <div className="right-panel">
         <h2 className="subtitle">Log In</h2>
-        <form className="login-form" onSubmit={handleSubmit}>
+        <form className="login-form">
           <div className="input-group">
-            <input type="text" id="username" name="username" placeholder="Username/Email" onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" id="username" name="username" placeholder="Username/Email"/>
           </div>
           <div className="input-group">
-            <input type="password" id="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" id="password" name="password" placeholder="Password"/>
           </div>
           <div className="input-group">
-            <input type="email" id="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="email" id="email" name="email" placeholder="Email" />
           </div>
           <button type="submit" className="submit-btn">Login</button>
         </form>
