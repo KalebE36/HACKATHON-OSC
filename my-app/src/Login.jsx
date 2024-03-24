8f82b0081890f442597504a415ee56b24732e555
ryder keeny <keenyryder@gmail.com>
2024-03-23 23:12:19 -0400

    react

---
+++ my-app/src/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+ import './Login.css';
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="left-panel">
+        <h1 className="title">CultureConnect</h1>
+      </div>
+      <div className="right-panel">
+        <h2 className="subtitle">Sign Up</h2>
+        <form className="login-form">
+          <div className="input-group">
+            <input type="text" id="username" name="username" placeholder="Username/Email" />
+          </div>
+          <div className="input-group">
+            <input type="password" id="password" name="password" placeholder="Password" />
+          </div>
+          <button type="submit" className="submit-btn">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
